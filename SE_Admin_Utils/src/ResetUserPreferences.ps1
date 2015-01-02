dcf3a8cd405d6afd616a349b81b044835fff4000
David Merritt <david.c.merritt@siemens.com>
2015-01-02 23:07:32 +0000

    updated copyright date

--- SE_Admin_Utils/src/ResetUserPreferences.ps1
+++ SE_Admin_Utils/src/ResetUserPreferences.ps1
@@ -1,5 +1,5 @@
 # ResetUserPreferences
-# Copyright (C) 2014, David C. Merritt, david.c.merritt@siemens.com
+# Copyright (C) 2014-2015, David C. Merritt, david.c.merritt@siemens.com
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
